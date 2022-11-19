ef0e34070826de59170d15db4d9d9f3221bcd846
Astromotive <lsheets@astromotive.space>
2022-11-19 09:27:52 -0800

    added rotation to vector class

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import math
 from vector import Vector
 from standard_atmosphere import Atmosphere
+from rk4 import *
+
+
+
+if __name__=="__main__":
+
+    # initial params
+    timestep     = 0.1 # [ms]
+    duration     = 2.0 # [s]
+
+    position     = Vector([0.0, 0.0, 0.0])
+    velocity     = Vector([0.0, 0.0, 0.0])
+    acceleration = Vector([0.0, 0.0, -9.8])
+
+
+    projectile = StateVector(position, velocity)
+    mass = 100 # kg
+    launch_angle = math.radians(30)
+
+
+
+
+    # numerical integration loop
+    for x in range( int(duration/timestep) ):
+        #
+        # could implement a function to vary acceleration here
+        #
+        for s, a in zip(state.vector, acceleration.points):
+            rk4(s, a, timestep)
--- vector.py
+++ vector.py
@@ -3,61 +3,73 @@ import math
 
 class Vector(object):
 
-    def __init__(self, points = [0.0, 0.0, 0.0]):
-        self.points = points
-        self.x      = self.points[0]
-        self.y      = self.points[1]
-        self.z      = self.points[2]
+    def __init__(self, *args):
+        if len(args) == 0:
+            self.data = [0,0,0]
+        elif len(args) == 1:
+            self.data = args[0]
+        elif len(args) == 2:
+            self.data = [args[0], args[1], 0]
+        elif len(args) == 3:
+            self.data = [args[0], args[1], args[2]]
+        self.x      = self.data[0]
+        self.y      = self.data[1]
+        self.z      = self.data[2]
 
 
     def sum(self, vector):
-        new = [x+y for x, y in zip(self.points, vector.points)]
-        return Vector3D(new)
+        new = [x+y for x, y in zip(self.data, vector.data)]
+        return Vector(new)
 
 
     def difference(self, vector):
-        new = [x-y for x, y in zip(self.points, vector.points)]
-        return Vector3D(new)
+        new = [x-y for x, y in zip(self.data, vector.data)]
+        return Vector(new)
 
 
     def add(self, vector):
-        for i, x in enumerate(vector.points):
-            self.points[i] += x
-        self.__init__(self.points)
+        for i, x in enumerate(vector.data):
+            self.data[i] += x
+        self.__init__(self.data)
 
 
     def subtract(self, vector):
-        for i, x in enumerate(vector.points):
-            self.points[i] -= x
-        self.__init__(self.points)
+        for i, x in enumerate(vector.data):
+            self.data[i] -= x
+        self.__init__(self.data)
+
+    def mult(self, scalar):
+        new = [x*scalar for x in self.data]
+        return Vector(new)
 
 
     def scale(self, scalar):
-        for x in range(len(self.points)):
-            self.points[x] *= scalar
-        self.__init__(self.points)
+        for x in range(len(self.data)):
+            self.data[x] *= scalar
+        self.__init__(self.data)
 
 
     def divide(self, scalar):
-        for x in range(len(self.points)):
-            self.points[x] /= scalar
-        self.__init__(self.points)
+        for x in range(len(self.data)):
+            self.data[x] /= scalar
+        self.__init__(self.data)
 
 
     def dot(self, vector):
-        new = sum([x*y for x, y in zip(self.points, vector.points)])
+        new = sum([x*y for x, y in zip(self.data, vector.data)])
         return new
+
 
 
     def cross(self, vector):
         x = self.y * vector.z - self.z * vector.y
         y = self.z * vector.x - self.x * vector.z
         z = self.x * vector.y - self.y * vector.x
-        return Vector3D([x,y,z])
+        return Vector([x,y,z])
 
 
     def copy(self):
-        return Vector3D(list(self.points))
+        return Vector(list(self.data))
 
 
     def magnitude(self):
@@ -70,12 +82,12 @@ class Vector(object):
             new = [0, 0, 0]
         else:
             new = [1/m * self.x, 1/m * self.y, 1/m * self.z]
-        return Vector3D(new)
+        return Vector(new)
 
 
     def inverse(self):
-        new = [-1 * x for x in self.points]
-        return Vector3D(new)
+        new = [-1 * x for x in self.data]
+        return Vector(new)
 
 
     def angle(self, vector):
@@ -90,6 +102,18 @@ class Vector(object):
         else:
             n   = dot / (m1 * m2)
         return math.acos(n)
+
+
+    def rotate(self, axis, angle):
+        vrot = self.mult(math.cos(angle))
+        v2 = axis.cross(self)
+        v2 = v2.mult(math.sin(angle))
+        v3 = axis.mult(axis.dot(self))
+        v3 = v3.mult(1 - math.cos(angle))
+        vrot.add(v2)
+        vrot.add(v3)
+        return vrot
+
 
 
     def update(self, array):
