5a5a68521d47ce3bdf580a6180557390a7942eaf
Astromotive <lsheets@astromotive.space>
2022-12-17 21:36:24 -0800

    nozzle

--- nozzle.py
+++ nozzle.py
@@ -4,6 +4,9 @@ from conversions import *
 from name_equals_main import imported
 from matplotlib import pyplot as plt
 
+R = R_univ # 8.314 J/mol K
+g = g_earth # 9.80665 m/s^2
+
 
 
 class Propellant(object):
@@ -13,133 +16,145 @@ class Propellant(object):
         self.mach = mach
         self.R = r
 
+Hydrogen = Propellant(2.01, 1.405, 1320, 4124)
 Helium = Propellant(4.0026, 1.667, 1020, 2077.1)
 Air = Propellant(28.97, 1.401, 343, 287)
 
 
-# choose monoprop gas
-gas = Helium
-
-R = R_univ # 8.314 J/mol K
-g = g_earth # 9.80665 m/s^2
 
 
-def throatTemp(Tc, gam):
-    return Tc*(1/(1+(gam-1)/2))
+class Nozzle(object):
+
+    def __init__(self, gas, Tc, Pc, Pe, T):
+        self.gas = gas
+        self.Tc = Tc
+        self.Pc = Pc
+        self.Pe = Pe
+        self.T = T
+        self.Cstar = self.calcCstar(gas.gam, gas.R, Tc)
+        self.isp = self.calcIsp(self.Cstar, gas.gam, Pe, Pc)
+        self.Wdot = T / (self.isp * g)
+        self.At = self.throatArea(self.Wdot, Pc, self.Cstar)
+        self.Pcns = self.pcns(self.Wdot, self.Cstar, self.At)
+        self.Ve = self.exitVelocity(gas.gam, gas.R, self.Tc, Pe, self.Pcns)
+        self.Te = self.exitTemp(Tc, Pe, self.Pcns, gas.gam)
+        self.ae = self.mach(self.Te, gas.gam, gas.R)
+        self.Me = self.machNum(self.Ve, self.ae)
+        self.Ae = self.exitArea(self.At, self.Me, gas.gam)
+        self.Tt = self.throatTemp(self.Tc, gas.gam)
+        self.Pt = self.throatPress(Pc, gas.gam)
+        self.Dt = 2*sqrt(self.At/pi)
+        self.De = 2*sqrt(self.Ae/pi)
 
 
-def throatPress(Pc, gam):
-    return Pc*(1+(gam-1)/2)**(-gam/(gam-1))
+    def throatTemp(self, Tc, gam):
+        return Tc*(1/(1+(gam-1)/2))
 
 
-def calcCstar(gam, R, T):
-    return sqrt(gam*R*T) / (gam * sqrt( (2/(gam+1))**((gam+1)/(gam-1)) ) )
+    def throatPress(self, Pc, gam):
+        return Pc*(1+(gam-1)/2)**(-gam/(gam-1))
 
 
-def calcIsp(cs, gam, pe, pc):
-    return (cs/g) * gam * sqrt((2/(gam-1))*(2/(gam+1))**((gam+1)/(gam-1))*(1-(pe/pc))**((gam-1)/gam))
+    def calcCstar(self, gam, R, T):
+        return sqrt(gam*R*T) / (gam * sqrt( (2/(gam+1))**((gam+1)/(gam-1)) ) )
 
 
-def throatArea(Wdot, Pc, cs):
-    return Wdot * cs * (1/Pc)
+    def calcIsp(self, cs, gam, pe, pc):
+        return (cs/g) * gam * sqrt((2/(gam-1))*(2/(gam+1))**((gam+1)/(gam-1))*(1-(pe/pc))**((gam-1)/gam))
 
 
-def mach(t, gam, r):
-    return sqrt(gam * r * t)
+    def throatArea(self, Wdot, Pc, cs):
+        return Wdot * cs * (1/Pc)
 
 
-def machNum(v, a):
-    return v / a
+    def mach(self, t, gam, r):
+        return sqrt(gam * r * t)
 
 
-def exitArea(At, Me, gam):
-    return At/Me * ( (1+(gam-1)/2*Me**2)/((gam+1)/2) )**((gam+1)/(2*(gam-1)))
+    def machNum(self, v, a):
+        return v / a
 
 
-def pcns(Wdot, cs, At):
-    return Wdot * cs * (1/(At))
+    def exitArea(self, At, Me, gam):
+        return At/Me * ( (1+(gam-1)/2*Me**2)/((gam+1)/2) )**((gam+1)/(2*(gam-1)))
 
 
-def exitVelocity(gam, r, t, pe, pc):
-    return sqrt( (2 * gam / (gam-1) ) * r * t * (1-(pe/pc)**((gam-1)/gam)) )
+    def pcns(self, Wdot, cs, At):
+        return Wdot * cs * (1/(At))
 
 
-def exitTemp(tcns, pe, pcns, gam):
-    return tcns * (pe / pcns)**((gam-1)/gam)
+    def exitVelocity(self, gam, r, t, pe, pc):
+        return sqrt( (2 * gam / (gam-1) ) * r * t * (1-(pe/pc)**((gam-1)/gam)) )
+
+
+    def exitTemp(self, tcns, pe, pcns, gam):
+        return tcns * (pe / pcns)**((gam-1)/gam)
+
+
+
 
 
 if not imported(__name__):
 
+    # choose monoprop gas
+    gas = Hydrogen
+
     arr_thrust = []
     arr_diameter = []
 
-    for _ in range(100):
+    # for _ in range(100):
+
+    Tc = 294 # K
+    Pc = psi2pascal( 5000)
+    T = lbf2newton( 100000 )
+    # T = lbf2newton( (_ * 100)+10000 )
+    Pe = atm * 2.5 # exit pressure
+
+    target = inch2meter(6)
+    Ae = 0
+    De = 0
+    error = target - De
+    deriv = 0
+    egain = 100
+    dgain = 1000
+    gain = 100
+
+    print( newton2lbf(T))
 
 
-        Tc = 294 # K
-        Pc = psi2pascal(2200)
-        # T = lbf2newton( 2000 )
-        T = lbf2newton( (_ * 100)+10000 )
-        Pe = atm * 3 # exit pressure
+    while error > 0.0001 :
 
-        target = inch2meter(7)
-        Ae = 0
-        De = 0
-        error = target - De
-        deriv = 0
-        egain = 100
-        dgain = 1000
-        gain = 100
+        Pe -= (error*egain + deriv*dgain) * gain
 
-        print( newton2lbf(T))
+        nozzle = Nozzle(gas, Tc, Pc, Pe, T)
+
+        deriv = error - (target-nozzle.De)
+        error = target - nozzle.De
+
+        arr_thrust.append( newton2lbf(T) )
+        arr_diameter.append( meter2inch(nozzle.Dt) )
 
 
-        while error > 0.0001 :
-
-            Pe -= (error*egain + deriv*dgain) * gain
-
-            Cstar = calcCstar(gas.gam, gas.R, Tc)
-            isp = calcIsp(Cstar, gas.gam, Pe, Pc)
-            Wdot = T / (isp * g)
-            At = throatArea(Wdot, Pc, Cstar)
-            Pcns = pcns(Wdot, Cstar, At)
-            Ve = exitVelocity(gas.gam, gas.R, Tc, Pe, Pcns)
-            Te = exitTemp(Tc, Pe, Pcns, gas.gam)
-            ae = mach(Te, gas.gam, gas.R)
-            Me = machNum(Ve, ae)
-            Ae = exitArea(At, Me, gas.gam)
-            Tt = throatTemp(Tc, gas.gam)
-            Pt = throatPress(Pc, gas.gam)
-            Dt = 2*sqrt(At/pi)
-            De = 2*sqrt(Ae/pi)
-
-            deriv = error - (target-De)
-            error = target - De
-
-            arr_thrust.append( newton2lbf(T) )
-            arr_diameter.append( meter2inch(Dt) )
-
-
-    plt.plot(arr_thrust, arr_diameter)
-    plt.grid()
-    plt.show()
+    # plt.plot(arr_thrust, arr_diameter)
+    # plt.grid()
+    # plt.show()
 
 
 
-    print("Chamber Temp: %.2f K" % Tc)
-    print("Throat Temp: %.2f K" % Tt)
-    print("Exit Temp: %.2f K" % Te)
-    print("Chamber Pressure: %.2f psi" % pascal2psi(Pc))
-    print("Throat Pressure: %.2f psi" % pascal2psi(Pt))
-    print("Exit Pressure: %.2f psi" % pascal2psi(Pe))
-    print("Cstar: %.2f m/s" % Cstar)
-    print("Isp: %.2f s" % isp)
-    print("Flow rate: %.2f kg/s" % Wdot)
-    print("Throat Area: %.6f sq.m" % At)
-    print("Throat Diameter: %.4f in" % meter2inch(Dt))
-    print("Exit Velocity: %.2f m/s" % Ve)
-    print("Exit Mach Number: %.2f" % Me)
-    print("Speed of Sound at Exit: %.2f m/s" % (Ve/Me))
-    print("Exit Area: %.6f sq.m" % Ae)
-    print("Exit Diameter: %.4f in" % meter2inch(De))
-    print("Area Ratio: %.2f" % (Ae/At))
+    print("Chamber Temp: %.2f K" % nozzle.Tc)
+    print("Throat Temp: %.2f K" % nozzle.Tt)
+    print("Exit Temp: %.2f K" % nozzle.Te)
+    print("Chamber Pressure: %.2f psi" % pascal2psi(nozzle.Pc))
+    print("Throat Pressure: %.2f psi" % pascal2psi(nozzle.Pt))
+    print("Exit Pressure: %.2f psi" % pascal2psi(nozzle.Pe))
+    print("Cstar: %.2f m/s" % nozzle.Cstar)
+    print("Isp: %.2f s" % nozzle.isp)
+    print("Flow rate: %.2f kg/s" % nozzle.Wdot)
+    print("Throat Area: %.6f sq.m" % nozzle.At)
+    print("Throat Diameter: %.4f in" % meter2inch(nozzle.Dt))
+    print("Exit Velocity: %.2f m/s" % nozzle.Ve)
+    print("Exit Mach Number: %.2f" % nozzle.Me)
+    print("Speed of Sound at Exit: %.2f m/s" % (nozzle.Ve/nozzle.Me))
+    print("Exit Area: %.6f sq.m" % nozzle.Ae)
+    print("Exit Diameter: %.4f in" % meter2inch(nozzle.De))
+    print("Area Ratio: %.2f" % (nozzle.Ae/nozzle.At))
